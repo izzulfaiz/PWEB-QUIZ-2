f80c80d6451022b22e9284a882e630313c5953ab
izzulfaiz <izzulfaiz.fi@gmail.com>
2021-05-15 16:06:26 +0700

    upload database

---
+++ data_user.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Bulan Mei 2021 pada 11.04
+-- Versi server: 10.4.17-MariaDB
+-- Versi PHP: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `data_user`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tabel_user`
+--
+
+CREATE TABLE `tabel_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `nama` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tabel_user`
+--
+
+INSERT INTO `tabel_user` (`id`, `username`, `password`, `nama`) VALUES
+(1, 'admin', '$2y$10$BYRiqx5Qa10VoBdSagbbAeqhDCAUU9wvORhzGESxTk2dZkWfsUkXW', 'izzul'),
+(3, 'izzul', '$2y$10$1n9EGFm0nPO69Cepk3v2VuSs7OQ5GiQC5XDk0kCoYpVBMsA9hBRNu', 'faiz'),
+(4, 'bro', '$2y$10$wE3iVMWpgY4mpCqHCM7ce.PYgrHqAt0pNimZZw5My4yXvJokVM9Xm', 'brody');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tabel_user`
+--
+ALTER TABLE `tabel_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tabel_user`
+--
+ALTER TABLE `tabel_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
